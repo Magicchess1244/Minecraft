b38c44956a0437898d30a57f151c366944fdcdda
Magicchess1244 <Niggasaurus@thehood.ngga>
2025-07-25 09:40:41 +0200

    wip v3

--- PerlinNoise.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PERLINNOISE_HPP__
-#define __PERLINNOISE_HPP__
-
-#include "common.hpp"
-
-double PerlinNoise(Vector3 Pos, int Octaves, double ConstFrequency);
-void SetGradients();
-
-#endif
