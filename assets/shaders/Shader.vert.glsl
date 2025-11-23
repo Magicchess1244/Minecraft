e0676a4be3171289f1fab1fa5c22f359ee5be50f
KONICHIGUA BEIBI <196363069+Magicchess1244@users.noreply.github.com>
2025-11-23 13:04:09 +0100

    Cherry-picked commit dfaa0c4

--- assets/shaders/Shader.vert.glsl
+++ assets/shaders/Shader.vert.glsl
@@ -1,18 +1,16 @@
 # version 460
 
 layout (location = 0) in vec3 a_position;
-layout (location = 1) in vec3 a_color;
-layout (location = 2) in vec2 a_uv;
-
+layout (location = 1) in vec4 a_color;
 layout (location = 0) out vec4 v_color;
-layout (location = 1) out vec2 v_uv;
 
-layout(set = 1, binding = 0) uniform MatrixBlock {
-    mat4 MVP;
-} u_matrices;
+// Define a uniform block
+//layout(std140, binding = 0) uniform MatrixBlock {
+//    mat4 projection;
+//} matrices;
 
+// Use it in your shader
 void main() {
-    gl_Position = u_matrices.MVP * vec4(a_position, 1.0);
-    v_color = vec4(a_color, 1.0);
-    v_uv = a_uv;
+    gl_Position = vec4(a_position, 1.0);
+    v_color = a_color;
 }
